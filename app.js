1638fed61a8212ab01822eacbfb46464438ae9c9
bhardwajabhi992 <74195762+bhardwajabhi992@users.noreply.github.com>
2021-09-02 12:17:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 const dbScript = require("./dbScripts");
 
-//creating for multer to define where restaurant images will be uploaded
-
-
-
 let db;
 
 //creating database connections
