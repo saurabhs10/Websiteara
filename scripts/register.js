a8dd4079dc86f42042257384f2787106c038bec6
vishalpaul768 <66180432+vishalpaul768@users.noreply.github.com>
2021-09-02 21:51:19 -0400

    Add files via upload

---
+++ scripts/register.js
@@ -0,0 +1,32 @@
+const form = document.getElementById("login-form");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirm-password");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (password.value != confirmPassword.value) {
+    alert("Password are not the same");
+    return;
+  }
+
+  fetch("http://localhost:3000/register-user", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email.value,
+      password: password.value,
+    }),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      alert(res.registerMsg);
+      if (res.registered) {
+        sessionStorage.setItem("res-email", email.value);
+        sessionStorage.setItem("res-password", password.value);
+        window.location = "/";
+      }
+    });
+});
