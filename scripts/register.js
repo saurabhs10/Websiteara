a8912c857e3b0f19345f4eca5198e0e8a0eb4b50
harnekaujla07 <74200927+harnekaujla07@users.noreply.github.com>
2021-09-08 21:03:24 -0400

    Add files via upload

--- scripts/register.js
+++ scripts/register.js
@@ -1,5 +1,6 @@
 const form = document.getElementById("login-form");
 const email = document.getElementById("email");
+const username = document.getElementById("username");
 const password = document.getElementById("password");
 const confirmPassword = document.getElementById("confirm-password");
 
@@ -17,6 +18,7 @@ form.addEventListener("submit", (e) => {
     },
     body: JSON.stringify({
       email: email.value,
+      username: username.value,
       password: password.value,
     }),
   })
@@ -24,6 +26,7 @@ form.addEventListener("submit", (e) => {
     .then((res) => {
       alert(res.registerMsg);
       if (res.registered) {
+        sessionStorage.setItem("res-username", username.value);
         sessionStorage.setItem("res-email", email.value);
         sessionStorage.setItem("res-password", password.value);
         window.location = "/";
---
+++ scripts/search.js
@@ -0,0 +1,39 @@
+const searchForm = document.getElementById("search-form");
+const searchQuery = document.getElementById("search-query");
+const loadingContainer = document.getElementById("loading-container");
+const timeDiv = document.getElementById("time-div");
+
+if (!resEmail) {
+  alert("You need to login to search a restaurant");
+  window.location = "/login";
+}
+
+restaurantsDiv.style.display = "none";
+timeDiv.style.display = "none";
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  loadingContainer.style.display = "flex";
+  search = searchQuery.value;
+  searching = true;
+
+  searchRestaurant("12:00")
+    .then((r) => r.json())
+    .then((r) => {
+      console.log(r);
+      bookingObj = r;
+      displayRestaurants(r);
+      if (r.length == 0) {
+        const h4 = document.createElement("h4");
+        h4.innerText = "No such restaurant or location found!";
+        h4.classList.add("tc");
+        h4.classList.add("mh");
+        h4.classList.add("no-bookings");
+
+        restaurantsDiv.replaceWith(h4);
+      } else {
+        timeDiv.style.display = "flex";
+      }
+      loadingContainer.style.display = "none";
+    });
+});
