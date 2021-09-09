034a2055557c83e7fedd939d9c7a73010447e9c1
vishalpaul768 <66180432+vishalpaul768@users.noreply.github.com>
2021-09-09 01:24:27 -0400

    Add files via upload

--- scripts/login-user.js
+++ scripts/login-user.js
@@ -1,11 +1,12 @@
 const loginText = document.getElementById("login-text");
 const resEmail = sessionStorage.getItem("res-email");
+const resUserName = sessionStorage.getItem("res-username");
 let tableName;
 if (resEmail) tableName = resEmail.split("@")[0];
-
+//console.log(resUserName);
 if (resEmail == "admin" && window.location != "http://localhost:3000/admin") {
   window.location = "/admin";
 } else if (resEmail) {
-  loginText.innerText = resEmail;
+  loginText.innerText = resEmail
   loginText.href = "/logout";
 }
