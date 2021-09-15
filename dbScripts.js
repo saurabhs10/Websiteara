6ad392261a6db88b2671ad46e90046ca20793b45
bhardwajabhi992 <74195762+bhardwajabhi992@users.noreply.github.com>
2021-09-15 12:17:31 -0400

    Add files via upload

--- dbScripts.js
+++ dbScripts.js
@@ -17,6 +17,12 @@ function createTable(db) {
   db.run(
     "CREATE TABLE IF NOT EXISTS restaurant(name text PRIMARY KEY, location text, tables int, price int, description text, image text)"
   );
+  // db.each(
+  //   "select name from sqlite_master where type='table'",
+  //   function (err, tables) {
+  //     console.log(tables);
+  //   }
+  // );
   //db.run("DROP TABLE abc");
 }
 
@@ -107,7 +113,27 @@ async function getRestaurants(db) {
   }
 }
 
+async function getRestaurantsByTime(db, row) {
+  try {
+    const result = await db.all("SELECT * FROM restaurant");
 
+    //will contain data for all restaurants
+    const resData = [];
+    for (let res of result) {
+      const resName = res.name.replace(/[^a-zA-Z0-9]/g, "");
+      const restaurant = await db.get(`SELECT * FROM ${resName} where time=?`, [
+        row.time,
+      ]);
+      restaurant.image = res.image;
+      resData.push(restaurant);
+    }
+
+    return resData;
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
+}
 
 async function getOneRestaurant(db, row) {
   try {
@@ -135,14 +161,91 @@ async function getUserPreviousBookings(db, row) {
   } catch (e) {
     if (!row.tableName)
       console.error(
-        ` `
+        `Previous booking error: Username not available, please login`
       );
     else console.error(`Previous booking error: ${e}`);
     return { restaurants: [], available: false };
   }
 }
 
+async function bookTable(db, row) {
+  try {
+    const userName = row.tableName.replace(/[^a-zA-Z0-9]/g, "");
+    await db.run(
+      `CREATE TABLE IF NOT EXISTS ${userName}(name text, booking_name text, time text, tables int, price int, description text, image text)`
+    );
 
+    const resName = row.name.replace(/[^a-zA-Z0-9]/g, "");
+    //reduce the number of restaurants so that the tables can be reduced from the restaurant table
+    //of the respective restaurant at the specfied time only
+    await db.run(`UPDATE ${resName} set tables = ? WHERE time=?`, [
+      row.tables - row.bookTables,
+      row.time,
+    ]);
+    const result = await db.run(
+      `INSERT INTO ${userName} VALUES (?, ?, ?, ?, ?, ?, ?)`,
+      [
+        row.name,
+        row.bookingName,
+        row.time,
+        Number.parseInt(row.bookTables),
+        row.price,
+        row.description,
+        row.image,
+      ]
+    );
+    if (result) return { booked: true };
+    return { booked: false };
+  } catch (e) {
+    console.error(`Booking error: ${e}`);
+    return { booked: false };
+  }
+}
+
+async function cancelTable(db, row) {
+  try {
+    //what this means is that if all tables are canceled remove the record
+    let result;
+    const userName = row.tableName.replace(/[^a-zA-Z0-9]/g, "");
+    //calculate new cost based on the number of tables canceled
+    const cancelPrice =
+      Number.parseInt(row.price / row.tables) * row.cancelTables;
+
+    if (row.tables == row.cancelTables) {
+      console.log(row);
+      result = await db.run(
+        `DELETE FROM ${userName} WHERE time = ? AND name = ?`,
+        row.time,
+        row.name
+      );
+    } else {
+      result = await db.run(
+        `UPDATE ${userName} SET tables = ?, price=?  WHERE name = ? AND time = ?`,
+        [row.tables - row.cancelTables, cancelPrice, row.name, row.time]
+      );
+    }
+
+    const resTable = row.name.replace(/[^a-zA-Z0-9]/g, "");
+    //fetching current tables available in the restaurant whose table is being canceled
+    let { tables } = await db.get(
+      `SELECT tables from ${resTable} where time=?`,
+      row.time
+    );
+    //increasing the tables with the number of tables canceled.
+    tables += row.cancelTables;
+
+    await db.run(`UPDATE ${resTable} set tables = ? WHERE time = ?`, [
+      tables,
+      row.time,
+    ]);
+
+    if (result) return { canceled: true };
+    return { canceled: false };
+  } catch (e) {
+    console.error(`Booking error: ${e}`);
+    return { canceled: false };
+  }
+}
 
 async function deleteRestaurant(db, row) {
   try {
@@ -150,6 +253,7 @@ async function deleteRestaurant(db, row) {
       `DELETE FROM restaurant WHERE name = ?`,
       row.restaurantName
     );
+
     const restaurantName = row.restaurantName.replace(/[^a-zA-Z0-9]/g, "");
     //drop the table that stores restaurant data according to time.
     await db.run(`DROP TABLE ${restaurantName}`);
@@ -163,7 +267,6 @@ async function deleteRestaurant(db, row) {
 
 async function updateRestaurant(db, row) {
   let exec;
-  console.log(row);
   try {
     exec = await db.run(
       "UPDATE restaurant SET location = ?, tables = ?, price = ?, description = ? WHERE name = ?",
@@ -177,6 +280,7 @@ async function updateRestaurant(db, row) {
     );
 
     const resTable = row.resName.replace(/[^a-zA-Z0-9]/g, "");
+    console.log(resTable);
     let startTime = 12;
     let endTime = 22;
     const incrementTables =
@@ -207,7 +311,27 @@ async function updateRestaurant(db, row) {
   }
 }
 
+async function searchRestaurant(db, row) {
+  try {
+    const names = await db.all(
+      `SELECT name, image FROM restaurant WHERE name LIKE '%${row.search}%' OR location LIKE '%${row.search}%'`
+    );
 
+    const rows = [];
+    for (let obj of names) {
+      const resTable = obj.name.replace(/[^a-zA-Z0-9]/g, "");
+      rows.push(
+        ...(await db.all(`SELECT * FROM ${resTable} WHERE time=?`, row.time))
+      );
+      rows[rows.length - 1].image = obj.image;
+    }
+
+    return rows;
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
+}
 
 module.exports = {
   init,
@@ -219,5 +343,9 @@ module.exports = {
   getUserPreviousBookings,
   deleteRestaurant,
   getOneRestaurant,
-  updateRestaurant
+  updateRestaurant,
+  bookTable,
+  cancelTable,
+  searchRestaurant,
+  getRestaurantsByTime,
 };
