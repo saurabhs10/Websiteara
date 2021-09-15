a4a54ae999af644c2f6cb596d64aa33322a21ef0
harnekaujla07 <74200927+harnekaujla07@users.noreply.github.com>
2021-09-14 21:10:41 -0400

    Add files via upload

--- scripts/login-user.js
+++ scripts/login-user.js
@@ -7,6 +7,7 @@ if (resEmail) tableName = resEmail.split("@")[0];
 if (resEmail == "admin" && window.location != "http://localhost:3000/admin") {
   window.location = "/admin";
 } else if (resEmail) {
-  loginText.innerText = resEmail
+  
+  loginText.innerText = resEmail;
   loginText.href = "/logout";
 }
