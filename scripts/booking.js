c66e26336ca91add6bbff27794b9fbe7255575b2
vishalpaul768 <66180432+vishalpaul768@users.noreply.github.com>
2021-09-15 16:37:54 -0400

    Delete booking.js

--- scripts/booking.js
+++
@@ -1,9 +0,0 @@
-//ask user to login to book a table and redirect to login page.
-if (!resEmail) {
-  alert("You need to login to book a table");
-  window.location = "/login";
-}
-
-fetchRestaurants()
-  .then((res) => res.json())
-  .then((res) => displayRestaurants(res));
